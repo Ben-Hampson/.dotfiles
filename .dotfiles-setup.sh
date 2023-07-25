55c1e792f94f2794608310e95ddf7904a1c16bab
Ben Hampson <email@email.com>
2023-07-25 16:15:13 +0100

    install meslo nerd font

--- .dotfiles-setup.sh
+++ .dotfiles-setup.sh
@@ -7,3 +7,7 @@ brew install gcc
 # zsh plugins
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Meslo Nerd Font - the nerd font recommended by Powerline10k
+brew tap homebrew/cask-fonts
+brew install font-meslo-lg-nerd-font

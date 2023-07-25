82b1dfeaa81e2b6c0ce1c5549fd6313e554cc97e
Ben Hampson <benjamin.hampson@tryg.dk>
2023-07-25 16:49:25 +0000

    brew install neovim

--- .dotfiles-setup.sh
+++ .dotfiles-setup.sh
@@ -4,6 +4,8 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 sudo apt-get install build-essential
 brew install gcc
 
+brew install neovim
+
 # zsh plugins
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting

d564e921f7468c02ee5dcb495bcefad3b068c155
Benjamin Hampson <benjamin.hampson@tryg.dk>
2023-07-25 15:49:47 +0000

    install FiraCode Nerd Font for linux or macos

--- .dotfiles-setup.sh
+++ .dotfiles-setup.sh
@@ -8,6 +8,17 @@ brew install gcc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# Meslo Nerd Font - the nerd font recommended by Powerline10k
-brew tap homebrew/cask-fonts
-brew install font-meslo-lg-nerd-font
+# FiraCode Nerd Font Light 
+unamestr=$(uname)
+if [["$unamestr" == "Darwin"]]; then
+	# Casks only work on macOS
+	brew tap homebrew/cask-fonts
+	brew install font-meslo-lg-nerd-font
+elif [["$unamestr" == "Linux"]]; then
+	wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Light/FiraCodeNerdFont-Light.ttf
+	sudo mv FiraCodeNerdFont-Light.ttf /usr/local/share/fonts
+	fc-cache -f -v
+fi
+
+echo "If this a remote session, you need to install the fonts on the client computer."
+echo "Change your terminal emulator's font to 'FiraCode Nerd Font'."

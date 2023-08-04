ab19dbc4cea13a7c84b3eb04e83e969c9553f12a
Ben Hampson <email@email.com>
2023-08-04 17:13:17 +0100

    add remote-sshfs.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,41 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-  And then you can explore or search through `:help lua-guide`
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
-
 -- disable netrw because we're using nvim-tree. This should be at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -220,6 +182,7 @@ require('lazy').setup({
   {'christoomey/vim-tmux-navigator'},
 
   {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+  {'nosduco/remote-sshfs.nvim'},
 }, {})
 
 -- [[ Setting options ]]
@@ -635,3 +598,6 @@ vim.diagnostic.config {
   signs = false,
   underline = false,
 }
+
+require('remote-sshfs').setup({})
+require('telescope').load_extension 'remote-sshfs'

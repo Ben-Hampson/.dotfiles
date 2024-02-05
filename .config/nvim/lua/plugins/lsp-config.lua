22648c9de35aaaf15a0c51cf817850403ae604cb
Ben Hampson <email@email.com>
2024-02-05 21:59:45 +0100

    improvement(neovim): get pyright to pick up poetry virtual env

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -16,8 +16,9 @@ return {
         -- Automatically installs these LSPs.
         -- Use the shortened names. Shown in light grey in :Mason.
         -- For list of lspconfigs available -> ':h lspconfig-all'
-        "pyright",  -- Python: for; go to definition, references, show function signature in hover
-        "ruff_lsp", -- Python: Used by none-ls for Formatting, linting
+
+        "pyright",  -- Python: go to definition, references, show function signature in hover
+        "ruff_lsp", -- Python: Used by none-ls -> Formatting, linting
         "lua_ls",
         "terraformls",
       },
@@ -32,8 +33,40 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
 
-      -- Must specify the LSPs here in order to use them.
-      lspconfig.pyright.setup({})
+      -- Must specify LSP configs below in order for them to work.
+
+      -- If poetry env is set up, get pyright to use that
+      local util = require('lspconfig/util')
+      local path = util.path
+
+      local function get_python_path(workspace)
+        -- Use activated virtualenv.
+        if vim.env.VIRTUAL_ENV then
+          return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+        end
+
+        -- Find and use virtualenv via poetry in workspace directory.
+        local match = vim.fn.glob(path.join(workspace, 'poetry.lock'))
+        if match ~= '' then
+          local venv = vim.fn.trim(vim.fn.system('poetry env info -p'))
+          return path.join(venv, 'bin', 'python')
+        end
+
+        -- Fallback to system Python.
+        return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
+      end
+
+      lspconfig.pyright.setup ({
+          on_attach = function()
+              require'lsp_signature'.on_attach {
+                  hint_enable = false,
+              }
+          end,
+          on_init = function(client)
+              client.config.settings.python.pythonPath = get_python_path(client.config.root_dir)
+          end
+      })
+
       lspconfig.ruff_lsp.setup({})
       lspconfig.lua_ls.setup({})
       lspconfig.terraformls.setup({})
@@ -60,16 +93,20 @@ return {
           vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, { buffer = ev.buf, desc = "LSP: Type definition" })
           vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = ev.buf, desc = "LSP: References" })
           vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = ev.buf, desc = "LSP: Implementations" })
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = ev.buf, desc = "LSP: Hover information about the symbol" })
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover,
+            { buffer = ev.buf, desc = "LSP: Hover information about the symbol" })
 
-          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, { buffer = ev.buf, desc = "LSP: Add workspace folder" })
-          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, { buffer = ev.buf, desc = "LSP: Remove workspace folder" })
+          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder,
+            { buffer = ev.buf, desc = "LSP: Add workspace folder" })
+          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder,
+            { buffer = ev.buf, desc = "LSP: Remove workspace folder" })
           vim.keymap.set('n', '<space>wl', function()
             print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
           end, { buffer = ev.buf, desc = "LSP: List workspace folders" })
 
           vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, { buffer = ev.buf, desc = "LSP: Rename symbol" })
-          vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, { buffer = ev.buf, desc = "LSP: Code action" })
+          vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action,
+            { buffer = ev.buf, desc = "LSP: Code action" })
           vim.keymap.set('n', '<space>f', function()
             vim.lsp.buf.format { async = true }
           end, { buffer = ev.buf, desc = "LSP: Format" })
